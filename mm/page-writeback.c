dc95135828472e970915e20a2a45c26ef4ce9472
biagio licari <biagiolicari7@gmail.com>
2013-05-04 15:49:42 +0200

    tweak page writeback

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -61,7 +61,7 @@ static inline long sync_writeback_pages(unsigned long dirtied)
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 55;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
